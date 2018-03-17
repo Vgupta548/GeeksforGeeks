4fb88d60bf2b0e485351e6a309ae6db6d03e52e0
Vipin Gupta <viping74@yahoo.com>
2018-03-17 10:45:24 +0530

    Program to reverse a stack using recursion
    we need a function that inserts at the bottom of a stack using the above given basic stack function.

---
+++ geeksForGeeks/com.ds.topics/stack/ReverseStackUsingRecursion.java
@@ -0,0 +1,30 @@
+class ReverseStackUsingRecursion{
+/*
+  * Program to reverse a stack using recursion. You are not allowed to use loop constructs like while, for..etc
+  *
+ */
+public static void main(String[] args){
+
+Stack<Integer> s = new Stack<Integer>();
+  s.push(1);
+  s.push(3);
+  s.push(3);
+  s.push(4);
+  s.push(5);
+  
+  ReverseStackUsingRecursion r = new ReverseStackUsingRecursion();
+  //Reverse stack
+  r.reverse();
+}
+
+//Recurvise Function to reverse stack
+private static void reverse(Stack<?> stack){
+  
+}
+
+//Function to put element at the bottom of stack
+private static void putAtBottom(Stack<?> s, Generic i>{
+
+}
+
+}
