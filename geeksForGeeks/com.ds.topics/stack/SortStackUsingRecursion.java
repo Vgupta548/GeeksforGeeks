48e25b6fcf7a758c2fb4591d4baaef073d12e7ce
Vipin Gupta <viping74@yahoo.com>
2018-03-17 11:09:57 +0530

    Sort a stack using recursion
    Hold all values in Function Call Stack until the stack becomes empty. When the stack becomes empty, insert all held items one by one in sorted order

---
+++ geeksForGeeks/com.ds.topics/stack/SortStackUsingRecursion.java
@@ -0,0 +1,31 @@
+
+/*
+  *Sort a stack using recursion
+  *
+*/
+class SortStackUsingRecursion{
+
+public static void main(String[] args){
+m 
+Stack<Integer> s = new Stack<Integer>();
+  s.push(-3);
+  s.push(6);
+  s.push(14);
+  s.push(8);
+  s.push(6);
+  s.push(-3);
+  s.push(-5);
+  sortStack(s);
+
+}
+//Function to create a call stack to reach at the bottom at stack
+private static void sortStack(Stack s){
+
+}
+
+
+//Function to insert new item at the sorted position of sorted stack
+private static void sortStack(Stack s, Integer i){
+
+}
+}
