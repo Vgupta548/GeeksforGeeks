3d593cfe48e84cbb9b67dbf2998d05a252d7d80f
Vipin Gupta <viping74@yahoo.com>
2018-03-17 16:41:15 +0530

    Design a stack with operations on middle element
     implement a stack which will support following operations in O(1) time complexity?
    1) push() which adds an element to the top of stack.
    2) pop() which removes an element from top of stack.
    3) findMiddle() which will return middle element of the stack.
    4) deleteMiddle() which will delete the middle element.
    The idea is to use Doubly Linked List (DLL). We can delete middle element in O(1) time by maintaining mid pointer. We can move mid pointer in both directions using previous and next pointers

---
+++ geeksForGeeks/com.ds.topics/stack/FindMiddleElementInStack.java
@@ -0,0 +1,54 @@
+class FindMiddleElementInStack{
+
+/*
+  * Deleting an element from middle is not O(1) for array. 
+  * Also, we may need to move the middle pointer up when we push an element and move down when we pop().
+  * In singly linked list, moving middle pointer in both directions is not possible.
+*/
+//The idea is to use Doubly Linked List (DLL)
+  class DubleLinkList{
+    DubleLinkList next;
+    DubleLinkList prev;
+    int data;
+  }
+  
+    public static void main(String[] arg){
+        FindMiddleElementInStack s = new FindMiddleElementInStack();
+        s.push(1);
+        s.push(15);
+        s.push(11);
+        s.push(14);
+        s.push(13);
+        s.findMiddleElement();
+        s.pop();
+        s.findMiddleElement();
+        s.push(81);
+        s.deleteMiddleElement();
+        s.findMiddleElement();
+        s.deleteMiddleElement();
+        s.pop();
+    }
+    
+    /*Function to push element in stack using DLL
+     and adjust DLL top pointer and middle Elemnt pointer accordingly */
+    private static void push(int i){
+    
+    }
+    
+    /*Function to pop element in stack using DLL
+     and adjust DLL top pointer and middle Elemnt pointer accordingly */
+    private static int pop(int i){
+    
+    }
+    
+   / *Function to return middle element in stack using DLL */
+    private static int findMiddleElement(){
+    
+    }
+    
+     / *Function to delete middle element in stack using DLL
+     and adjust middle Elemnt pointer accordingly */
+    private static void deleteMiddleElement(){
+    
+    }
+}
