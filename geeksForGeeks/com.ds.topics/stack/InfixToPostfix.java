aaee1c91aad1b5e9e583b9c9ed1115b85633b95a
Vipin Gupta <viping74@yahoo.com>
2018-03-18 19:37:57 +0530

    Infix to Postfix a +b *c +d => abc*+d+.
    Algorithm
    1. Scan the infix expression from left to right.
    2. If the scanned character is an operand, output it.
    3. Else,
    …..3.1 If the precedence of the scanned operator is greater than the precedence of the operator in the stack(or the stack is empty), push it.
    …..3.2 Else, Pop the operator from the stack until the precedence of the scanned operator is less-equal to the precedence of the operator residing on the top of the stack. Push the scanned operator to the stack.
    4. If the scanned character is an ‘(‘, push it to the stack.
    5. If the scanned character is an ‘)’, pop and output from the stack until an ‘(‘ is encountered.
    6. Repeat steps 2-6 until infix expression is scanned.
    7. Pop and output from the stack until it is not empty

---
+++ geeksForGeeks/com.ds.topics/stack/InfixToPostfix.java
@@ -0,0 +1,23 @@
+/*
+*Infix TO Prefix Expression
+*/
+
+class InfixToPostfix{
+
+  /* Method For Infix To Postfix */
+  public static void main(String[] args){
+    String s = "A+B*C+D";
+    s = ChangeInfixToPreFix(s);
+  }
+
+  /* Function To Change Infix Expression To PostfixExpression Using Stack
+   *@param Infix String
+   *@retur PostFix String
+  */
+  public String ChangeInfixToPreFix(String s){
+
+    return s;
+  }
+
+
+}
