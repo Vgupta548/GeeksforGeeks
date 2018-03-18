3f90932983b7e708cf24af152dd2d5801b494ce7
Vipin Gupta <viping74@yahoo.com>
2018-03-18 18:22:31 +0530

    Efficiently implement k stacks in a single array
    Method 1 (Divide the array in slots of size n/k) 
    Method 2 (A space efficient implementation)
    The idea is to use two extra arrays for efficient implementation of k stacks in an array. This may not make much sense for integer stacks, but stack items can be large for example stacks of employees, students, etc where every item is of hundreds of bytes. For such large stacks, the extra space used is comparatively very less as we use two integer arrays as extra space.
    
    Following are the two extra arrays are used:
    1) top[]: This is of size k and stores indexes of top elements in all stacks.
    2) next[]: This is of size n and stores indexes of next item for the items in array arr[]. Here arr[] is actual array that stores k stacks.
    Together with k stacks, a stack of free slots in arr[] is also maintained. The top of this stack is stored in a variable ‘free’.
    
    All entries in top[] are initialized as -1 to indicate that all stacks are empty. All entries next[i] are initialized as i+1 because all slots are free initially and pointing to next slot. Top of free stack, ‘free’ is initialized as 0.

---
+++ geeksForGeeks/com.ds.topics/stack/KStackArray.java
@@ -0,0 +1,53 @@
+/*
+  * Create a data structure kStacks that represents k stacks.
+  * k stacks should use the same array for storing elements. Following functions must be supported by kStacks.
+
+  * push(int x, int sn) –> pushes x to stack number ‘sn’ where sn is from 0 to k-1
+  * pop(int sn) –> pops an element from stack number ‘sn’ where sn is from 0 to k-1
+*/
+
+class KStackArray{
+
+  /* Mathod to test k stacks in a single array */
+  public static void main(String[] args){
+    MutiStackArray stacks = new MutiStackArray(4, 12);
+    stack.push
+  
+  }
+  
+  //Data structure kStacks that represents k stacks 
+  class MutiStackArray{
+    int k; //no of stacks
+    int n; //array Size;
+    int[] array //array that holds staks elemrnt
+    MutiStackArray(int k, int n){
+      this.k = k;
+      this.n = n;
+      array = new int[n];
+    }
+    
+     /* Method to Push element 'i' in 'sn' no stack */  
+      private void push(int sn, int i){
+
+      }
+
+  /* Method to pop element from stack 'sn' */
+      private int pop(int sn){
+
+      }
+      
+    /* Method to check if array is Full or not */
+      private boolean isFull(int sn){
+
+      }
+      
+    /* Method to cheek if array is Empty or not*/
+      private boolean isEmpty(int sn){
+
+      }
+  
+  }
+
+ 
+
+}
