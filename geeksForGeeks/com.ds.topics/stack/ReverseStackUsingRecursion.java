517f89434d9658d6713c763e635927102a4f1112
v1dew1 <viping74@yahoo.com>
2018-03-20 12:22:25 +0530

    statck problems 4

--- geeksForGeeks/com.ds.topics/stack/ReverseStackUsingRecursion.java
+++ geeksForGeeks/com.ds.topics/stack/ReverseStackUsingRecursion.java
@@ -1,3 +1,6 @@
+package stack;
+import java.util.Stack;
+
 class ReverseStackUsingRecursion{
 /*
   * Program to reverse a stack using recursion. You are not allowed to use loop constructs like while, for..etc
@@ -5,26 +8,26 @@ class ReverseStackUsingRecursion{
  */
 public static void main(String[] args){
 
-Stack<Integer> s = new Stack<Integer>();
-  s.push(1);
-  s.push(3);
-  s.push(3);
-  s.push(4);
-  s.push(5);
-  
-  ReverseStackUsingRecursion r = new ReverseStackUsingRecursion();
-  //Reverse stack
-  r.reverse();
-}
-
-//Recurvise Function to reverse stack
-private static void reverse(Stack<?> stack){
-  
-}
-
-//Function to put element at the bottom of stack
-private static void putAtBottom(Stack<?> s, Generic i>{
-
-}
+	Stack<Integer> s = new Stack<Integer>();
+	  s.push(1);
+	  s.push(3);
+	  s.push(3);
+	  s.push(4);
+	  s.push(5);
+	  
+	  ReverseStackUsingRecursion r = new ReverseStackUsingRecursion();
+	  //Reverse stack
+	  //r.reverse();
+	}
+	
+	//Recurvise Function to reverse stack
+	private static void reverse(Stack stack){
+	  
+	}
+	
+	//Function to put element at the bottom of stack
+	private static void putAtBottom(Stack s, int i){
+	
+	}
 
 }
