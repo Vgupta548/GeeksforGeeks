486bb72e9d9487f78d1d0534c6040d8a1a4210d4
vipin GUpta <viping74@yahoo.com>
2018-03-27 19:37:42 +0530

    Trapping rain water problem solution by array

--- geeksForGeeks/com.ds.topics/array/TrappingRainWater.java
+++ geeksForGeeks/com.ds.topics/array/TrappingRainWater.java
@@ -18,19 +18,62 @@ public class TrappingRainWater {
 		
 		int[] containerHeight = new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7};
 		System.out.println("----------------------------------------------------");
-		long maxArea = totalWater_v2(containerHeight);
-		maxArea = totalWater_v2(containerHeight);
-		System.out.println("maxArea "+maxArea);
+		long maxArea1 = totalWater_v2(containerHeight);
+		long maxArea11 = totalWater_v1(containerHeight);
+		System.out.println("maxArea1 "+maxArea1);
 		System.out.println("----------------------------------------------------");
 		containerHeight = new int[]{10,8,1,1,1,1,1,1,2,1};
-		maxArea = totalWater_v2(containerHeight);
-		System.out.println("maxArea "+maxArea);
+		long maxArea2 = totalWater_v2(containerHeight);
+		long maxArea22 = totalWater_v1(containerHeight);
+		System.out.println("maxArea2 "+maxArea2);
+		
+		System.out.println("maxArea1 "+maxArea1);
+		System.out.println("maxArea11 "+maxArea11);
+		System.out.println("maxArea2 "+maxArea2);
+		System.out.println("maxArea22 "+maxArea22);
 
 
 	}
-
+	
 	/**
-	 * This Method is Used For
+	 * This Method is Used For calculating total water tapped by two array
+	 * @param h
+	 * @return
+	 */
+	
+	private static long totalWater_v1(int[] h) {
+		int[] left = new int[h.length];
+		int[] right = new int[h.length];
+		
+		int max = left[0] = h[0];
+		for(int i=1; i< h.length; i++){
+			if(max<h[i]) max=h[i];
+			left[i] = max;
+		}
+		
+		printArray(left);
+		 max = right[h.length-1] = h[h.length-1];
+		for(int i=h.length-2; i>=0; i--){
+			if(max<h[i]) max=h[i];
+			right[i] = max;
+		}
+		printArray(right);
+		int area = 0;
+		for(int i =0; i<h.length; i++){
+			area += Math.min(left[i], right[i]);
+		}
+		return area;
+	}
+
+	private static void printArray(int[] a){
+		System.out.println(" ");
+		for(int i=0; i<a.length;i++){
+			System.out.print(" "+ a[i]);
+		}
+		System.out.println(" ");
+	}
+	/**
+	 * This Method is Used For calculating total water tapped by stack
 	 * @param h
 	 * @return
 	 */
